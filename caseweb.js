3b6ba461e3920cc1ff0483064d340dc2adf67989
Saida Reddy <saidareddy@iic.com>
2023-08-24 23:31:12 +0530

    Adding new & changes

---
+++ caseweb.js
@@ -0,0 +1,8 @@
+function beforesubmit(){
+    let outputdata = document.querySelector(".outputdata");
+    let inputdata = document.querySelector(".inputdata");
+    console.log("inputdata.value",inputdata.value);
+    
+    let dateformat = new Date(inputdata.value).toLocaleDateString("en-IN");
+    outputdata.value = dateformat;
+}
