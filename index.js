f1a6e23a7f7fd8bf2182f9a417e055a5f236b97e
Saida Reddy <saidareddy@iic.com>
2023-08-24 08:32:52 +0530

    adding and modify

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 function beforesubmit(){
-    let outputdata = document.querySelector(".outputdata");
-    let inputdata = document.querySelector(".inputdata");
-    console.log("inputdata.value",inputdata.value);
+let outputdata = document.querySelector(".outputdata");
+let inputdata = document.querySelector(".inputdata");
+console.log("inputdata.value",inputdata.value);
 
-    let dateformat = new Date(inputdata.value).toLocaleDateString("en-IN");
-    outputdata.value = dateformat;
+let dateformat = new Date(inputdata.value).toLocaleDateString("en-IN");
+outputdata.value = dateformat;
 
 }
+function timestamp() { 
+var response = document.getElementById("g-recaptcha-response"); 
+if (response == null || response.value.trim() == "") {
+    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } 
+    setInterval(timestamp, 500);
