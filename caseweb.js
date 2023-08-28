628597e3cef30f8ff647ea42d88b73e9be119f74
Saida Reddy <saidareddy@iic.com>
2023-08-28 14:00:29 +0530

    modify

--- caseweb.js
+++ caseweb.js
@@ -6,3 +6,4 @@ function beforesubmit(){
     let dateformat = new Date(inputdata.value).toLocaleDateString("en-IN");
     outputdata.value = dateformat;
 }
+function timestamp() { var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } setInterval(timestamp, 500);
