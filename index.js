9c047b7a4c8dd9c112a2bbb8357c4d27a3bcb894
Saida Reddy <saidareddy@iic.com>
2023-09-10 23:22:48 +0530

    modify

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-$("form").submit(function(event) {
-    var recaptcha = $("#g-recaptcha-response").val();
-    if (recaptcha === "") {
-       event.preventDefault();
-       alert("Please check the recaptcha");
-    }
- });
+let captchachecked = false;
+debugger;  
 
 function beforesubmit(){
+if (captchachecked) {
 let outputdata = document.querySelector(".outputdata");
 let inputdata = document.querySelector(".inputdata");
 console.log("inputdata.value",inputdata.value);
@@ -14,11 +10,19 @@ console.log("inputdata.value",inputdata.value);
 let dateformat = new Date(inputdata.value).toLocaleDateString("en-IN");
 outputdata.value = dateformat;
 
+}else{
+alert("please check the reCAPTCHA box to submit the form");
+   event.preventDefault();
+}
 }
 function timestamp() { 
 var response = document.getElementById("g-recaptcha-response"); 
 if (response == null || response.value.trim() == "") {
-    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-    elems["ts"] = JSON.stringify(new Date().getTime());
-    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } 
-    setInterval(timestamp, 500);
+   var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+   elems["ts"] = JSON.stringify(new Date().getTime());
+   document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } } 
+   setInterval(timestamp, 500);
+   function captchasuccess() {
+   captchachecked = true;
+   }
+   
